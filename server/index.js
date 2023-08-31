bc211095402927aacd2ce643b7122808f0592925
SOHAM PATHAK <120130000+pathaksoham2003@users.noreply.github.com>
2023-08-31 14:38:30 +0530

    Update index.js
    fndfgn

--- server/index.js
+++ server/index.js
@@ -10,9 +10,7 @@ db();
 const socket = require("socket.io");
 const cors = require("cors");
 app.use(express.json());
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-}));
+app.use(cors());
 
 app.use("/api/auth" , userRoutes);
 app.use("/api/messages" , messageRoute);
