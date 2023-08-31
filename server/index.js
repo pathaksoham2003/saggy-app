989a42eec9fad3be169d9eb0dcc8bb6ccb07d778
SOHAM PATHAK <120130000+pathaksoham2003@users.noreply.github.com>
2023-08-31 16:58:11 +0530

    Update index.js
    rev

--- server/index.js
+++ server/index.js
@@ -10,10 +10,7 @@ db();
 const socket = require("socket.io");
 const cors = require("cors");
 app.use(express.json());
-app.use(cors({
-        origin: process.env.FRONTEND_URL,
-        credentials:true,
-    }));
+app.use(cors());
 
 app.use("api/auth" , userRoutes);
 app.use("api/messages" , messageRoute);
