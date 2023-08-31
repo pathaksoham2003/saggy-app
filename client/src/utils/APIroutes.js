69a5faf4767f9eb0e52ae3a71ca0bae4d967eb6d
SOHAM PATHAK <120130000+pathaksoham2003@users.noreply.github.com>
2023-08-31 17:15:38 +0530

    Update APIroutes.js
    wow!

--- client/src/utils/APIroutes.js
+++ client/src/utils/APIroutes.js
@@ -1,7 +1,7 @@
 export const host = import.meta.env.VITE_SERVER_URL;
 
-export const registerRoute = `${host}/api/auth/register`;
-export const loginRoute = `${host}/api/auth/login`;
-export const allContactsRoute = `${host}/api/auth/allusers`;
-export const sendMessageRoute = `${host}/api/messages/addmsg`;
-export const getMessageRoute = `${host}/api/messages/getmsg`;
+export const registerRoute = `${host}api/auth/register`;
+export const loginRoute = `${host}api/auth/login`;
+export const allContactsRoute = `${host}api/auth/allusers`;
+export const sendMessageRoute = `${host}api/messages/addmsg`;
+export const getMessageRoute = `${host}api/messages/getmsg`;
